840726617f7c509fdab9469051e1002c575df4a1
marian12345 <marian.zwarg@gmx.de>
2023-11-09 14:58:32 +0100

    fix ghpages baseURL

--- dist/assets/AboutView-e630d0b9.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,c as t,p as _,a as c,b as a}from"./index-03d196b1.js";const d={},n=e=>(_("data-v-6ee4f2ed"),e=e(),c(),e),p={class:"about"},i=n(()=>a("h1",null,"This is an about page",-1)),r=[i];function u(e,f){return s(),t("div",p,r)}const h=o(d,[["render",u],["__scopeId","data-v-6ee4f2ed"]]);export{h as default};
