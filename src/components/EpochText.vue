ad3435676fe34981c98c167fd8ef4268072fe739
marian12345 <marian.zwarg@gmx.de>
2025-12-04 16:54:37 +0100

    add lorem ipsum text

--- src/components/EpochText.vue
+++ src/components/EpochText.vue
@@ -48,7 +48,17 @@ getData();
     <p class="sourceLink">Quelle: {{ summarySourceLink }}</p>
   </div>
   <div v-else-if="loading && !error">
-    <p>Loading...</p>
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
+    </p>
   </div>
   <div v-else>
     <p>
