4bdb07487e9047ae1ab4780c2e137beff1f834ef
marian12345 <marian.zwarg@gmx.de>
2026-02-11 16:05:53 +0100

    build for gh-pages

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,10 @@ import BackendLoadingComponent from "@/components/BackendLoadingComponent.vue";
 import { useBackendStatus } from "@/composables/useBackendStatus";
 
 const { isBackendReady } = useBackendStatus();
-//!isBackendReady
 </script>
 
 <template>
-  <BackendLoadingComponent v-if="true" />
+  <BackendLoadingComponent v-if="!isBackendReady" />
   <RouterView v-else />
 </template>
 
--- src/components/BackendLoadingComponent.vue
+++ src/components/BackendLoadingComponent.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import theAwakening from "@/assets/gifs/theAwakening.gif";
 </script>
 
